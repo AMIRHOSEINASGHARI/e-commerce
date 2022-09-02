586cae288d7e589ec54c6cd828e9896bc55c755a
Amirhosein Asghari <amirhoseinasghari2000@gmail.com>
2022-09-02 16:02:05 +0430

    ProductDetails component styles

--- src/components/ProductDetails/Productdetails.js
+++ src/components/ProductDetails/Productdetails.js
@@ -28,38 +28,71 @@ const ProductDetails = () => {
   if (Object.keys(product).length === 0) return <Loader />;
   return (
     <div>
-      <div>
-        <div>
-          <img src={image} alt={title} />
+      <div className="lg:flex lg:items-center lg:justify-center mt-16">
+        <div className="flex py-10 mb-3 lg:mr-8 items-center justify-center">
+          <img
+            className="w-60 h-60 lg:w-64 lg:h-64 xl:w-80 xl:h-80 bg-cover mb-3"
+            src={image}
+            alt={title}
+          />
         </div>
-        <div>
-          <h6>${price}</h6>
-          <h6>{category}</h6>
-          <h5>{shortTitle(title)}</h5>
-          <p>{description}</p>
-          <div>
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-            <AiOutlineStar />
+        <div className="mx-10">
+          <h6 className="font-light w-fit text-blue-800 bg-blue-50 rounded-lg px-3 py-1 mb-2">
+            ${price}
+          </h6>
+          <h6 className="font-light capitalize">{category}</h6>
+          <h5 className="mb-4 font-extrabold text-xl text-slate-500">
+            {shortTitle(title)}
+          </h5>
+          <p className="font-light lg:w-[350px] xl:w-[450px]">{description}</p>
+          <div className="flex mt-3 mb-6">
+            <AiFillStar className="fill-amber-400" />
+            <AiFillStar className="fill-amber-400" />
+            <AiFillStar className="fill-amber-400" />
+            <AiFillStar className="fill-amber-400" />
+            <AiOutlineStar className="fill-amber-400" />
           </div>
-          <button>buy now</button>
-          <Link to={`/`}>
-            <IoCaretBackOutline />
-            <span>back to shop</span>
+          <button className="uppercase font-extrabold bg-blue-500 text-white hover:bg-blue-600 transition-all duration-200 rounded-full py-3 px-10 mb-5">
+            buy now
+          </button>
+          <Link
+            className="flex items-center w-fit justify-center border rounded-md border-gray-200 text-gray-500 capitalize px-5 py-3"
+            to={`/`}
+          >
+            <IoCaretBackOutline className="mr-2 -ml-2 text-2xl" />
+            <span className="font-bold">back to shop</span>
           </Link>
         </div>
       </div>
-      <div>
-        <h1>
-          more <span>{category}</span> products
+      <div className="flex items-center justify-center">
+        <h1 className="capitalize text-center mt-14 mb-10 w-fit mx-10 py-3 text-3xl border-b font-extralight">
+          more{" "}
+          <span
+            className={`${
+              category === "men's clothing"
+                ? "text-emerald-500"
+                : category === "jewelery"
+                ? "text-orange-500"
+                : category === "electronics"
+                ? "text-yellow-500"
+                : category === "women's clothing"
+                ? "text-purple-500"
+                : ""
+            }  capitalize font-bold`}
+          >
+            {category}
+          </span>{" "}
+          products
         </h1>
       </div>
       {filteredProducts.length === 0 ? (
         <Loader />
       ) : (
-        <div>
+        <div
+          className={`max-w-[1200px] m-5 px-5 mx-auto grid gap-5 xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:${
+            filteredProducts.length === 3 ? "grid-cols-3" : "grid-cols-4"
+          }`}
+        >
           {filteredProducts.map((el) => (
             <CardElement
               key={el.id}
