05147dcf9382e328bf57285eecba1fd0e4f9897b
Amirhosein Asghari <amirhoseinasghari2000@gmail.com>
2022-09-02 14:58:10 +0430

    category list component

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 //contexts
 import { ProductsContext } from "../../context/ProductsContextProvider";
 //components
@@ -6,11 +6,23 @@ import CardElement from "./CardElement";
 
 const HomePage = () => {
   const products = useContext(ProductsContext);
+  const [newProducts, setNewProducts] = useState([]);
+  const categories = ["all", ...new Set(products.map((el) => el.category))];
   return (
     <div>
-      {products.map((el) => (
-        <CardElement {...el} key={el.id} />
-      ))}
+      <div>
+        <h1>here is all {categories.length - 1} categories</h1>
+      </div>
+      <div>
+        {categories.map((el, i) => (
+          <button key={i}>{el}</button>
+        ))}
+      </div>
+      <div>
+        {products.map((el) => (
+          <CardElement {...el} key={el.id} />
+        ))}
+      </div>
     </div>
   );
 };
